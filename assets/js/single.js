bac31beb6cc8143a542c72d62e561b9d831e3ee4
JosephRule <josephrule1@gmail.com>
2022-03-03 11:05:10 -0800

    error handling for single repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -15,7 +30,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("there was a problem with your request!")
+            document.location.replace("./index.html");
+
         }
     });
 
@@ -63,4 +79,5 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
+
+getRepoName();
